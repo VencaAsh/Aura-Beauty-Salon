42560b5678596c97eae6df6bc6bc0c60df7e5aaa
mrkindesign <113291279+mrkindesign@users.noreply.github.com>
2025-05-18 23:31:06 +0200

    Favicon

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -69,9 +69,17 @@ export const SITE_METADATA = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-icon.png' },
+      { url: '/favicon/apple-touch-icon.png' },
+    ],
+    other: [
+      {
+        rel: 'manifest',
+        url: '/favicon/site.webmanifest',
+      },
     ],
   },
 };
